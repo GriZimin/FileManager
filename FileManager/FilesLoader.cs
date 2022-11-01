fae8dc7ee52f1febcc6ae3104bdcdf0944ba6520
GriZimin <grizimin@gmail.com>
2022-11-01 21:55:55 +0500

    Добавьте файлы проекта.

---
+++ FileManager/FilesLoader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FileManager
+{
+    public class FilesGetter
+    {
+        public FilesGetter()
+        {
+
+        }
+        public List<DirectoryInfo> GetDirs(string path)
+        {
+            List<DirectoryInfo> res = new List<DirectoryInfo>();
+            try
+            {
+                DirectoryInfo fileList = new DirectoryInfo(path);                
+                DirectoryInfo[] dirs = fileList.GetDirectories(); //get all directories
+
+                for (int i = 0; i < dirs.Length; i++)
+                {
+                    res.Add(dirs[i]);
+                }                
+            }
+            catch
+            {
+
+            }
+            return res;
+        }
+        public List<FileInfo> GetFiles(string path)
+        {
+            List<FileInfo> res = new List<FileInfo>();
+            try
+            {
+                DirectoryInfo fileList = new DirectoryInfo(path);
+                FileInfo[] files = fileList.GetFiles(); //get all files                
+                
+                for (int i = 0; i < files.Length; i++)
+                {
+                    res.Add(files[i]);
+                }
+            }
+            catch
+            {
+
+            }
+            return res;
+        }
+    }    
+}
---
+++ FileManager/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+
+namespace FileManager
+{    
+    public partial class MainWindow : Window
+    {
+        List<string[]> buttonsSettings = new List<string[]>();
+        FilesGetter loader = new FilesGetter();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AddFilesToList(string path)
+        {                       
+            MainListBox.Items.Clear();
+            var dirList = loader.GetDirs(path);
+            var fileList = loader.GetFiles(path);
+            for(int i = 0; i < dirList.Count; i++)
+            {
+                MainListBox.Items.Add(dirList[i]);                
+            }
+            for (int i = 0; i < fileList.Count; i++)
+            {
+                MainListBox.Items.Add(fileList[i]);
+            }
+        }      
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PathTextBox.Text = @"C:\";
+            AddFilesToList(@"C:\");                
+        }
+
+        private void Find_Click(object sender, RoutedEventArgs e)
+        {
+            AddFilesToList(PathTextBox.Text);
+        }        
+
+        private void MainListBox_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (Path.GetExtension(Path.Combine(PathTextBox.Text,MainListBox.SelectedItem.ToString())) == "")
+            {
+                PathTextBox.Text = Path.Combine(PathTextBox.Text,MainListBox.SelectedItem.ToString());
+                AddFilesToList(PathTextBox.Text);
+            }
+            else
+            {
+                Process.Start(Path.Combine(PathTextBox.Text, MainListBox.SelectedItem.ToString()));
+            }
+        }
+    }    
+}
